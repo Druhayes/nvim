4fee511b8c6b33990bcca6ef610350a212d663f5
Drew Hayes <dru.hayes@gmail.com>
2025-04-09 20:57:56 -0600

    Added lazygit and stock debug
    Using the stock debug with kickstart instead of a custom one.

--- init.lua
+++ init.lua
@@ -601,10 +601,17 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        pyright = {},
-        -- rust_analyzer = {},
+        clangd = {},
+        gopls = {},
+        pyright = {
+          settings = {
+            python = {
+              analysis = { diagnosticMode = 'off', typeCheckingMode = 'off' },
+            },
+          },
+        },
+
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -825,28 +832,36 @@ require('lazy').setup({
     end,
   },
 
+  -- {
+  --   -- 'lunarvim/darplus.nvim',
+  --   -- require 'darkplus.nvim',
+  --   vim.cmd 'colorscheme darkplus',
+  -- },
+
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'folke/tokyonight.nvim', --'lunarvim/darkplus.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
+      require('tokyonight').setup { --tokyonight
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
       }
-
+      --
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- Lua
+      -- vim.cmd [[colorscheme darkplus]],
+      vim.cmd.colorscheme 'tokyonight-night' --
     end,
   },
-
+  --
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
@@ -922,10 +937,10 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
